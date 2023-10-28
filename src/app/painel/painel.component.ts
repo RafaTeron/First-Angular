c6c3d180b4646428c6c42f2b829a1a81c3d4a557
RafaTeron <rafael_sabreu@hotmail.com>
2023-10-28 18:11:09 -0300

    Importando frases(Painel)

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Frase } from '../shared/frase.model';
+import { FRASE } from './frase.mock';
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.scss']
 })
 export class PainelComponent {
+
+  public frases: Array<Frase> = FRASE
+
+  constructor() {
+    console.log(this.frases)
+  }
 
 }
