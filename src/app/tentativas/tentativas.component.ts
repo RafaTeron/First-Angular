c8aec8e6682509c118adfeadfda6b059cbd70834
RafaTeron <rafael_sabreu@hotmail.com>
2023-10-29 21:52:52 -0300

    Criando a classe coracao

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Coracao } from '../shared/coracao.model';
+
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
@@ -9,5 +11,9 @@ export class TentativasComponent {
 
   public coracaoCheio : string = '/assets/coracao_cheio.png'
   public coracaoVazio : string = '/assets/coracao_vazio.png'
+
+  public coracao: Array<Coracao> = [
+    new Coracao(true),new Coracao(true),new Coracao(true)
+  ]
 
 }
