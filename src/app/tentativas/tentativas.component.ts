1fe94701d645383f5f0fb031d4a9a09ab921f822
RafaTeron <rafael_sabreu@hotmail.com>
2023-11-05 07:24:29 -0300

    Controlando tentativas (parte 3 Eliminando Tentativas)

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -20,6 +20,13 @@ export class TentativasComponent implements OnInit, OnChanges {
     console.log(this.coracoes)
   }
   ngOnChanges (): void {
+
+    if(this.tentativas !== this.coracoes.length){
+      let indice = this.coracoes.length - this.tentativas
+
+      this.coracoes[indice - 1].cheio = false 
+    }
+
     console.log('tentativas recebidas do painel : ' + this.tentativas)
   }
 
